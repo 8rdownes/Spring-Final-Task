ab692523340dff2091a135f8794b0a50ccf98b5f
8rdownes <31481017+8rdownes@users.noreply.github.com>
2018-05-01 09:18:07 -0500

    Add files via upload

--- task.js
+++ task.js
@@ -21,7 +21,12 @@ const TaskExport = function(){
     //You must reference the function above to create them each time
     //make all times range from 0 to 15 and all values range from 0 to 100
     function Factory(n){
-
+      let list[];
+for (let a=0; a<n a++;){
+let t=createTast(rInt(15), rInt(100));
+  list.push(t);
+}
+return list;
     }
 
     //do not change anything below there!
